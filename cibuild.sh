85ad6c28bee28606df1c585f2443197fb5ed78c2
renanmpimentel <renan.pimentel@gmail.com>
2015-10-26 21:57:06 -0200

    [config] tentativa de fazer deploy automatico na branch gh-pages

--- cibuild.sh
+++ cibuild.sh
@@ -6,3 +6,13 @@ echo 'Testing travis...'
 bundle exec travis-lint
 bundle exec jekyll build
 bundle exec htmlproof ./_site
+
+# config
+git config --global user.email "blog@devparana.org"
+git config --global user.name "Blog DevParana"
+
+# deploy
+git init
+git add .
+git commit -m "Deploy to GitHub Pages"
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
