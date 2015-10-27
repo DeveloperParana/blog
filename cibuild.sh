1099e2926235ca6de699368bb9bc024802657ccc
renanmpimentel <renan.pimentel@gmail.com>
2015-10-26 23:05:42 -0200

    [config] adicionando Gemfile.lock

--- cibuild.sh
+++ cibuild.sh
@@ -17,4 +17,4 @@ echo "Deploy"
 git init
 git add .
 git commit -m "Deploy to GitHub Pages"
-git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
+echo "git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1"
