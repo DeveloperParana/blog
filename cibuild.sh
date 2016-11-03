1aec8d8ed70cd3220b27b2ef166c7a1f29680afd
Fernando Fabricio dos Santos <ffsantos@uolinc.com>
2016-11-03 12:48:19 -0200

    alterando cibuild

--- cibuild.sh
+++ cibuild.sh
@@ -7,14 +7,14 @@ bundle exec travis-lint
 bundle exec jekyll build
 bundle exec htmlproof ./_site --disable-external
 
-# config
-echo "Git config"
-git config --global user.email "blog@devparana.org"
-git config --global user.name "Blog DevParana"
+## config
+#echo "Git config"
+#git config --global user.email "blog@devparana.org"
+#git config --global user.name "Blog DevParana"
 
 # deploy
-echo "Deploy"
-git init
-git add --all
-git commit -m "Deploy to GitHub Pages"
-git push --force --quiet "https://${GH_TOKEN}@github.com/DeveloperParana/blog.git" master:gh-pages
+#echo "Deploy"
+#git init
+#git add --all
+#git commit -m "Deploy to GitHub Pages"
+#git push --force --quiet "https://${GH_TOKEN}@github.com/DeveloperParana/blog.git" master:gh-pages
